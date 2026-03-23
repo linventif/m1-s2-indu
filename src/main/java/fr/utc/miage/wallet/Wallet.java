096b37736d1d9077edae0a69f2913d1c7f3a9041
linventif <gregoire.becue@pm.me>
2026-03-23 16:08:03 +0100

    refact: remove transcation and simplify wallet

--- src/main/java/fr/utc/miage/wallet/Wallet.java
+++ src/main/java/fr/utc/miage/wallet/Wallet.java
@@ -1,47 +1,30 @@
 package fr.utc.miage.wallet;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 class Wallet {
+  private HashMap<Action, Integer> listAction;
 
-  private String code;
-  private ArrayList<Transaction> transactions;
-  private Map<Action, Integer> listAction;
-
-  public Wallet(final String code) {
-    this.code = code;
-    this.transactions = new ArrayList<Transaction>();
-    this.listAction = new HashMap<Action, Integer>();
-  }
-
-  public String getCode() {
-    return this.code;
-  }
-
-  public ArrayList<Transaction> getTransactions() {
-    return this.transactions;
+  public Wallet() {
+    this.listAction = new HashMap<>();
   }
 
   public Map<Action, Integer> getListAction() {
     return this.listAction;
   }
 
-  public void addTransaction(Transaction transaction) {
-    Action action = transaction.getAction();
-    this.transactions.add(transaction);
-
-    if (!this.listAction.containsKey(action))
-      this.listAction.put(action, transaction.getQuantity());
-    else
-      this.listAction.put(action, this.listAction.get(action) + transaction.getQuantity());
+  public void addAction(Action action, int quantity) {
+    if (quantity < 0) {
+      throw new IllegalArgumentException("Quantity cannot be negative");
+    }
+    listAction.put(action, listAction.getOrDefault(action, 0) + quantity);
   }
 
   @Override
   public String toString() {
-    return "Wallet [getCode()=" + getCode() + ", getTransactions()=" + getTransactions() + ", getListAction()="
-        + getListAction() + "]";
+    return "Wallet{" +
+        "listAction=" + listAction +
+        '}';
   }
-
 }
